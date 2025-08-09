1bfa22afaf3300d11170d589ccc4cb2086f80c0e
lovesniikira <161968044+lovesniikira@users.noreply.github.com>
2025-08-09 21:28:38 +0200

    Update script.js

--- animated navigation/script.js
+++ animated navigation/script.js
@@ -3,3 +3,4 @@ const nav = document.getElementById("nav");
 
 toggle.addEventListener("click", () => nav.classList.toggle("active"));
 toggle.addEventListener("click", () => nav.classList.toggle("active"));
+090909090
